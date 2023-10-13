b8f6eb5780ead8628cc4b74682599da1b07bef4a
smodha26 <sunny.modha@gmail.com>
2023-10-12 22:29:03 -0400

    creation of JsonDatabaseHandler.java with a test account + creation of JsonDatabaseHandler.java file to store the logic behind the manipulation of the json file

---
+++ src/JsonDatabaseHandler.java
@@ -0,0 +1,16 @@
+public class JsonDatabaseHandler {
+
+    // Write code to read JSON file
+
+
+
+    // Write code to create a new account in the json file
+
+    // Demander a chatGPT comment store les infos du individual dans le singleton
+
+
+    // TEST
+    public static void main(String[] args) {
+
+    }
+}
