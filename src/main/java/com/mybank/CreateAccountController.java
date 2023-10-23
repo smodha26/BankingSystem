e5ae0bce4df4e5344aa1acd36bb1ac7b65243276
smodha26 <sunny.modha@gmail.com>
2023-10-22 23:16:45 -0400

    LoginUI opens when account created successfully

--- src/main/java/com/mybank/CreateAccountController.java
+++ src/main/java/com/mybank/CreateAccountController.java
@@ -43,6 +43,7 @@ public class CreateAccountController {
 
                     jsonDataManager.createAccount(username,password);
 
+                    openLoginUI();
                     createAccountUI.getFrame().dispose();
 
                 }
